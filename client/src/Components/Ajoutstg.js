aefa6acbf2e3204402335afb2dc991dbf9e2576b
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2022-06-01 17:21:59 +0000

    create form for AjoutStg file

--- client/src/Components/Ajoutstg.js
+++ client/src/Components/Ajoutstg.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import "./Ajoutstg.css";
 
 function Ajoutstg() {
   return (
-    <div>About</div>
-  )
+    <div className='ajout__stg'>
+      <Form className='form'>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>CNE *</Form.Label>
+          <Form.Control type='text' />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Nom</Form.Label>
+          <Form.Control type='text' />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Prenom</Form.Label>
+          <Form.Control type='text' />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Filiére</Form.Label>
+          <Form.Control type='text' />
+        </Form.Group>
+        <Form.Group className='btns'>
+          <Button variant='primary' type='submit'>
+            Ajouter Stagiaire
+          </Button>
+          <Button variant='outline-primary' type='submit'>
+            Spectacle Stagiaires
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
 
-export default Ajoutstg
+export default Ajoutstg;
