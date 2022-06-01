7720cb9c610f6f48d0ddda105cacc665a338fb26
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2022-06-01 21:15:30 +0000

    Add intern to DataBase

--- client/src/Components/Ajoutstg.js
+++ client/src/Components/Ajoutstg.js
@@ -1,30 +1,89 @@
 import React from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { useState } from "react";
+import Axios from "axios";
+import QRCode from "react-qr-code";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "react-bootstrap";
 import "./Ajoutstg.css";
 
 function Ajoutstg() {
+  const [cne, setCne] = useState("");
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [filiere, setFiliere] = useState("");
+
+  const [stagiaireList, setStagiaireList] = useState([]);
+
+  // Add intern to DataBase
+  const addStagiaire = () => {
+    Axios.post("http://localhost:3001/create", {
+      cne: cne,
+      nom: nom,
+      prenom: prenom,
+      filiere: filiere,
+    }).then(() => {
+      setStagiaireList([
+        ...stagiaireList,
+        {
+          cne: cne,
+          nom: nom,
+          prenom: prenom,
+          filiere: filiere,
+        },
+      ]);
+    });
+  };
+
   return (
     <div className='ajout__stg'>
       <Form className='form'>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>CNE *</Form.Label>
-          <Form.Control type='text' />
+          <Form.Control
+            type='text'
+            onChange={(event) => {
+              setCne(event.target.value);
+            }}
+          />
         </Form.Group>
 
         <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Nom</Form.Label>
-          <Form.Control type='text' />
+          <Form.Control
+            type='text'
+            onChange={(event) => {
+              setNom(event.target.value);
+            }}
+          />
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Prenom</Form.Label>
-          <Form.Control type='text' />
+          <Form.Control
+            type='text'
+            onChange={(event) => {
+              setPrenom(event.target.value);
+            }}
+          />
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Filiére</Form.Label>
-          <Form.Control type='text' />
+          <Form.Control
+            type='text'
+            onChange={(event) => {
+              setFiliere(event.target.value);
+            }}
+          />
         </Form.Group>
         <Form.Group className='btns'>
-          <Button variant='primary' type='submit'>
+          <Button variant='primary' type='submit' onClick={addStagiaire}>
             Ajouter Stagiaire
           </Button>
           <Button variant='outline-primary' type='submit'>
@@ -32,6 +91,76 @@ function Ajoutstg() {
           </Button>
         </Form.Group>
       </Form>
+
+
+      s{/* Containers */}
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant='top' src='holder.js/100px180?text=Image cap' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className='list-group-flush'>
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href='#'>Card Link</Card.Link>
+                <Card.Link href='#'>Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant='top' src='holder.js/100px180?text=Image cap' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className='list-group-flush'>
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href='#'>Card Link</Card.Link>
+                <Card.Link href='#'>Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant='top' src='holder.js/100px180?text=Image cap' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className='list-group-flush'>
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href='#'>Card Link</Card.Link>
+                <Card.Link href='#'>Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -14,41 +14,22 @@ const db = mysql.createConnection({
   password: "startaymz1598753",
   database: "node_mysql_ts",
 
-  host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    // host: process.env.DB_HOST,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_DATABASE
 });
 
 app.post("/create", (req, res) => {
-  const code = req.body.code;
-  const name = req.body.name;
-  const age = req.body.age;
-  const country = req.body.country;
-  const position = req.body.position;
-  const wage = req.body.wage;
+  const cne = req.body.cne;
+  const nom = req.body.nom;
+  const prenom = req.body.prenom;
+  const filiere = req.body.filiere;
 
   db.query(
-    "INSERT INTO employees (code, name, age, country, position, wage) VALUES (?,?,?,?,?,?)",
-    [code, name, age, country, position, wage],
-    (err, result) => {
-      if (err) {
-        //console.log(err);
-      } else {
-        res.send("Values Inserted");
-      }
-    }
-  );
-});
-
-//////////////////
-
-app.post("/insertDate/:code", (req, res) => {
-  code = req.params.code;
-  db.query(
-    "INSERT INTO employees (Entree) SELECT TO_CHAR(systimestamp,'DD-MON-YYYY HH24:MI:SS') from dual where code = ?",
-    code,
+    "INSERT INTO stagiaire (cne, nom, prenom, filiere) VALUES (?,?,?,?)",
+    [cne, nom, prenom, filiere],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -59,110 +40,127 @@ app.post("/insertDate/:code", (req, res) => {
   );
 });
 
+//////////////////
+
+// app.post("/insertDate/:code", (req, res) => {
+//   code = req.params.code;
+//   db.query(
+//     "INSERT INTO employees (Entree) SELECT TO_CHAR(systimestamp,'DD-MON-YYYY HH24:MI:SS') from dual where code = ?",
+//     code,
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.send("Values Inserted");
+//       }
+//     }
+//   );
+// });
 
 
-app.get("/employees", (req, res) => {
+
+// app.get("/employees", (req, res) => {
   
-  db.query("SELECT * FROM employees"  , (err, result) => {
-    if (err) {
-      //console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+//   db.query("SELECT * FROM employees"  , (err, result) => {
+//     if (err) {
+//       //console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
 
-// This code working with Qr code Scanner, to Get employees from DB 
-app.get("/getEmp/:code", (req, res) => {
-  code = req.params.code;
-  db.query("SELECT * FROM employees where code =?" , code , (err, result) => {
-    if (err) {
-      //console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+// // This code working with Qr code Scanner, to Get employees from DB 
+// app.get("/getEmp/:code", (req, res) => {
+//   code = req.params.code;
+//   db.query("SELECT * FROM employees where code =?" , code , (err, result) => {
+//     if (err) {
+//       //console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
 
 
 
-app.get("/users/:code", (req, res) => {
+// app.get("/users/:code", (req, res) => {
   
-  db.query("SELECT Entree FROM employees where code = ? ", code , (err, result) => {
-    if (err) {
-      //console.log(err);
-    } else {
-      if (!result) {return res.send("Insert First")}  
-      else if (result) {return end()   } else {res.send("You are Already Done!")} 
-    }
-  });
-});
+//   db.query("SELECT Entree FROM employees where code = ? ", code , (err, result) => {
+//     if (err) {
+//       //console.log(err);
+//     } else {
+//       if (!result) {return res.send("Insert First")}  
+//       else if (result) {return end()   } else {res.send("You are Already Done!")} 
+//     }
+//   });
+// });
 
 
-  app.put("/updateEnterDate/:code", (req, res) => {
-    const code = req.params.code;
-    const d = moment().format('MMMM Do YYYY, h:mm:ss a')
-    db.query(
-      "UPDATE employees SET Entree = ? WHERE code = ?",
-      [d, code],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(result);
-        }
-      }
-    );
-  });
+//   app.put("/updateEnterDate/:code", (req, res) => {
+//     const code = req.params.code;
+//     const d = moment().format('MMMM Do YYYY, h:mm:ss a')
+//     db.query(
+//       "UPDATE employees SET Entree = ? WHERE code = ?",
+//       [d, code],
+//       (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           res.send(result);
+//         }
+//       }
+//     );
+//   });
 
-  app.put("/updateOutDate/:code", (req, res) => {
-    const code = req.params.code;
-    db.query(
-      "UPDATE employees SET Sortit = ? WHERE code = ?",
-      [moment().format('MMMM Do YYYY, h:mm:ss a'), code],
-      (err, result) => {
-        if (err) {
-          //console.log(err);
-        } else {
-          res.send(result);
-        }
-      }
-    );
-  });
-
-
-
+//   app.put("/updateOutDate/:code", (req, res) => {
+//     const code = req.params.code;
+//     db.query(
+//       "UPDATE employees SET Sortit = ? WHERE code = ?",
+//       [moment().format('MMMM Do YYYY, h:mm:ss a'), code],
+//       (err, result) => {
+//         if (err) {
+//           //console.log(err);
+//         } else {
+//           res.send(result);
+//         }
+//       }
+//     );
+//   });
 
 
 
 
-// QR Reader
-app.put("/update", (req, res) => {
-  const id = req.body.id;
-  const wage = req.body.wage;
-  db.query(
-    "UPDATE employees SET wage = ? WHERE id = ?",
-    [wage, id],
-    (err, result) => {
-      if (err) {
-        //console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
-});
 
-app.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
-    if (err) {
-      //console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+
+
+// // QR Reader
+// app.put("/update", (req, res) => {
+//   const id = req.body.id;
+//   const wage = req.body.wage;
+//   db.query(
+//     "UPDATE employees SET wage = ? WHERE id = ?",
+//     [wage, id],
+//     (err, result) => {
+//       if (err) {
+//         //console.log(err);
+//       } else {
+//         res.send(result);
+//       }
+//     }
+//   );
+// });
+
+// app.delete("/delete/:id", (req, res) => {
+//   const id = req.params.id;
+//   db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+//     if (err) {
+//       //console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("Yey, your server is running on port 3001");
